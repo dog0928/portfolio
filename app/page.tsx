b7e041ec7835434cb55ba69f9893aecea86a5c7b
dog0928 <dogtatinakama@gmail.com>
2026-04-20 11:36:25 +0900

    add base

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,126 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <>
+      <div>
+        <p>Portfolio</p>
+        <h1>KotaroHachiro</h1>
+        <ul>
+          <li>data: 2008/09/28</li>
+          <li>hobby: programming, music, game</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Skills</h2>
+        <ul>
+          <h3>Frontend</h3>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>Tailwind CSS</li>
+          <li>ChakraUI</li>
+          <li>Shadcn</li>
+          <li>Next.js</li>
+          <li>Vite</li>
+        </ul>
+        <ul>
+          <h3>Backend</h3>
+          <li>TypeScript</li>
+          <li>JavaScript</li>
+          <li>Go</li>
+          <li>Hono</li>
+          <li>Express</li>
+          <li>Next.js</li>
+          <li>Flask</li>
+          <li>Gin</li>
+        </ul>
+        <ul>
+          <h3>Database</h3>
+          <li>MySQL</li>
+          <li>PostgreSQL</li>
+          <li>Firebase</li>
+        </ul>
+        <ul>
+          <h3>Tools</h3>
+          <li>Git</li>
+          <li>GitHub</li>
+          <li>Docker</li>
+          <li>Supabase</li>
+          <li>Proxmox VE</li>
+          <li>PufferPanel</li>
+          <li>Auth0</li>
+          <li>Neovim</li>
+          <li>Canva</li>
+          <li>Adobe</li>
+          <li>OpenAI API</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <h3>Portfolio</h3>
+            <p>MySite</p>
+            <li>React</li>
+            <li>Next.js</li>
+          </li>
+          <li>
+            <h3>Boostudy</h3>
+            <p>学生から社会人までが使える学習支援アプリ</p>
+            <a href="https://boostudy.app">https://boostudy.app</a>
+            <li>React</li>
+            <li>Flask</li>
+            <li>Hono</li>
+            <li>PostgreSQL</li>
+            <li>Supabase</li>
+            <li>Docker</li>
+            <li>OpenAI API</li>
+          </li>
+          <li>
+            <h3>ZenClear</h3>
+            <p>レポートをもう、溜め込まない。レポートスケジュール管理拡張機能</p>
+            <a href="https://github.com/dog0928/zen_clear">https://github.com/dog0928/zen_clear</a>
+            <li>JavaScript</li>
+          </li>
+          <li>
+            <h3>ConneLab Portfolio</h3>
+            <p>ConneLabのポートフォリオ</p>
+            <a href="https://www.connelab.jp">https://www.connelab.jp</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Milestones</h2>
+        <ul>
+          <li>
+            <h3>2026</h3>
+            <h4>「Boostudy」リリース</h4>
+            <h4>流しそうめん同好会会長就任</h4>
+            <h4>未踏Jr応募 第一次選考落ち</h4>
+            <h4>学内コンペティション アプリ開発部門 「ZenClear」 銅賞 メンター特別賞受賞</h4>
+            <h4>「puffer_discrd」リリース</h4>
+          </li>
+          <li>
+            <h3>2025</h3>
+            <h4>異能Vation応募</h4>
+            <h4>学内ビジネスコンテスト応募 第二次選考落ち</h4>
+            <h4>ボランティア: 「チルON!べ~す」の活動に参加</h4>
+            <h4>N高グループ: 磁石際企画の制作</h4>
+            <h4>N高グループ: 投資部 第七期 ADV生</h4>
+            <h4>プロダクト開発 講義受講</h4>
+            <h4>学内コンペティション Web開発部門 入賞</h4>
+            <h4>ご当地ゆるスポーツアワード 学内全国発表通過</h4>
+          </li>
+          <li>
+            <h3>2024</h3>
+            <h4>流しそうめん同好会運営就任</h4>
+          </li>
+          <li>
+            <h3>2023</h3>
+            <h4>塾講師就任</h4>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
